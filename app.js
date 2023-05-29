e2363f5117f77fb44e385744d7e940dd95b674cd
zafarkh <hatamovzafar7070@gmail.com>
2023-05-29 09:10:59 +0500

    'd'

--- app.js
+++ app.js
@@ -1,112 +1,35 @@
-// (true && false);  //false
-// (true || false);  //true
-// (!true || !false);  //true
-// (true && !false); //true
-// (!true || 1); //1
-// (!(true && false)); //true
-// ((true && false) || true) //true
-// (100 && -1) //
-// ((true || false) && !true) //false
-// ((true && true && false) || false) //false
-// (1 == true && false == 0) //
-// (true != 0 && false == !-1 ) // true
-
-// console.log(name);
-// var name = 'Dave'
-// var name = 'Mike'
-
-// const PI = 3.14
-
-// function fun(arg) {
-// 	arg = 'hi'
-// 	console.log(arg);
-// }
-
-// fun(PI)
-
-// const arr = [['name', 'John'], ['age', 44], ['gender', 'male']]
-
-// var age = 23
-// let ism = 'Nick'
-// var job = 'Frontend developer'
-// const aim = "Happiness"
-
-// function fun() {
-// 	const player = "leo"
-// }
-
-// if(22) {
-// 	let statue = "Senior Developer"
-// }
-
-// console.log(window);
-
-// const age = {yosh: 22}
-// const age2 = age
-// age2.yosh = 33
-
-// console.log(age2);
-
-// let squad = ['leo', 'robert', ['usman', 'rafinha'], 'pablo']
-// let subs = squad[2]
-// squad[2] = ['ferran']
-// console.log(subs);
-// console.log(squad[2]);
-
-// let obj = {
-// 	name: 'John',
-// 	age: 44,
-// 	gender: 'male'
-// }
-
-// function fun(arg) {
-// 	let array = []
-// 	for (const key in arg) {
-// 		let insideArr = []
-//     let arrFirst = key;
-//     let arrValue = arg[key]
-// 		insideArr.push(key);
-//     insideArr.push(arg[key]);
-// 		array.push(insideArr)
-//   }
-// 	console.log(array);
-// }
-
-// fun(obj)
-
-// const arrPer = [['name', 'john'], ['age', 33], ['status', 'wealthy']]
-
-// const objPer = {
-// 	name: 'john',
-// 	age: 33,
-// 	status: 'wealthy'
-// }
-
-// console.log(Object.entries(objPer))
-
-// function convert(arg) {
-// 	let arr = []
-// 	for (const key in arg) {
-// 		let insideArr = []
-// 		insideArr.push(key)
-// 		insideArr.push(arg[key])
-// 		arr.push(insideArr)
+// function fn () {
+// 	this.name = 'Eshmat';
+// 	const obj = {
+// 		name: 'Toshmat',
+// 		fn1() {
+// 			return this.name
+// 		},
+// 		fn2: function() {
+// 			return this.name
+// 		},
+// 		fn3: () => {
+// 			return this.name
+// 		}
 // 	}
-// 	console.log(arr);
-// 	return arr
+// 		return obj;
 // }
 
-// convert(objPer)
+// console.log(fn().fn1());
+// console.log(fn().fn2());
+// console.log(fn().fn3());
 
-// function myFun (arg) {
-// 	let obj = {}
-// 	for (let index of arg) {
-// 		let key = index[0]
-// 		let value = index[1]
-// 		obj[key] = value
-// 	}
-// 	console.log(obj);
-// 	return obj
+// function fn(a, b) {
+// 	console.log(a, b);
 // }
 
-// myFun(arrPer)
+// const arr = [2,5,7]
+
+// fn(...arr)
+
+// function fn (arr) {
+// 	console.log(...arr);
+// }
+// const arr2 = [3,6,7]
+
+// fn(arr2)
