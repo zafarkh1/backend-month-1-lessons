f712c5d835dd3c4f4f0715ab345233cea487e0f2
zafarkh <hatamovzafar7070@gmail.com>
2023-05-29 23:40:38 +0500

    'd'

--- app.js
+++ app.js
@@ -33,3 +33,27 @@
 // const arr2 = [3,6,7]
 
 // fn(arr2)
+
+// let operand = (5).toString(2);
+// let result = ~operand
+// console.log(operand);
+// console.log(result);
+
+// console.log((4).toString(2) >> 2);
+// 0000 0100 => 4 ~ 10000
+
+// let result = 321.999999999999
+// let result = '1232eedfv3'
+// let result = '3e3eeee'
+// let result = "22 33 44";
+// let result = " 99 years";
+
+// console.log(parseInt(result));
+
+// let result = "32 dfoisnmfnfsklnmdflknmd";
+// let result = "e 22";
+// console.log(parseFloat(result));
+
+// (function() {
+// 	console.log('Nnu');
+// })()
