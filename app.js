c488cb6c2ba691d13c9e57e69343dfc28c35bda3
zafarkh <hatamovzafar7070@gmail.com>
2023-06-22 07:58:17 +0500

    test

--- app.js
+++ app.js
@@ -1,112 +1,17 @@
-// (true && false);  //false
-// (true || false);  //true
-// (!true || !false);  //true
-// (true && !false); //true
-// (!true || 1); //1
-// (!(true && false)); //true
-// ((true && false) || true) //true
-// (100 && -1) //
-// ((true || false) && !true) //false
-// ((true && true && false) || false) //false
-// (1 == true && false == 0) //
-// (true != 0 && false == !-1 ) // true
+//                      lesson-9
 
-// console.log(name);
-// var name = 'Dave'
-// var name = 'Mike'
+// process.cwd() => current working directory
+// fs.unlinkSync(path) => fileni o'chirib yuboradi
+// fs.mkdirSync('./test', { recursive : true}) => yangi folder ochib beradi
 
-// const PI = 3.14
 
-// function fun(arg) {
-// 	arg = 'hi'
-// 	console.log(arg);
-// }
+//                     lesson-10
+// authentication vs authorization node js
+//1chi qism - algoritm , 2 - payload (id), 3 - secret
+// randomBytes
 
-// fun(PI)
+// const { log } = require('console');
+// const accessToken = sign ({id : 1}, '!QER#LJNF')
 
-// const arr = [['name', 'John'], ['age', 44], ['gender', 'male']]
-
-// var age = 23
-// let ism = 'Nick'
-// var job = 'Frontend developer'
-// const aim = "Happiness"
-
-// function fun() {
-// 	const player = "leo"
-// }
-
-// if(22) {
-// 	let statue = "Senior Developer"
-// }
-
-// console.log(window);
-
-// const age = {yosh: 22}
-// const age2 = age
-// age2.yosh = 33
-
-// console.log(age2);
-
-// let squad = ['leo', 'robert', ['usman', 'rafinha'], 'pablo']
-// let subs = squad[2]
-// squad[2] = ['ferran']
-// console.log(subs);
-// console.log(squad[2]);
-
-// let obj = {
-// 	name: 'John',
-// 	age: 44,
-// 	gender: 'male'
-// }
-
-// function fun(arg) {
-// 	let array = []
-// 	for (const key in arg) {
-// 		let insideArr = []
-//     let arrFirst = key;
-//     let arrValue = arg[key]
-// 		insideArr.push(key);
-//     insideArr.push(arg[key]);
-// 		array.push(insideArr)
-//   }
-// 	console.log(array);
-// }
-
-// fun(obj)
-
-// const arrPer = [['name', 'john'], ['age', 33], ['status', 'wealthy']]
-
-// const objPer = {
-// 	name: 'john',
-// 	age: 33,
-// 	status: 'wealthy'
-// }
-
-// console.log(Object.entries(objPer))
-
-// function convert(arg) {
-// 	let arr = []
-// 	for (const key in arg) {
-// 		let insideArr = []
-// 		insideArr.push(key)
-// 		insideArr.push(arg[key])
-// 		arr.push(insideArr)
-// 	}
-// 	console.log(arr);
-// 	return arr
-// }
-
-// convert(objPer)
-
-// function myFun (arg) {
-// 	let obj = {}
-// 	for (let index of arg) {
-// 		let key = index[0]
-// 		let value = index[1]
-// 		obj[key] = value
-// 	}
-// 	console.log(obj);
-// 	return obj
-// }
-
-// myFun(arrPer)
+// console.log(verify(accessToken, "!QER#LJNF"));
+// const { sign, verify } = require("jsonwebtoken");
