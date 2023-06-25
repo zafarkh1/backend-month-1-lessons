49836292e68d3ce27c9278184e75c2e7a27c2785
zafarkh <hatamovzafar7070@gmail.com>
2023-06-25 23:08:06 +0500

    's'

--- app.js
+++ app.js
@@ -16,3 +16,45 @@
 // npm i express
 // app.use(express.json()) => node.js dagi req.on(chunk) va JSON parse ogirish jarayonini bajarib beradi
 // /books/:id => patch da id berish
+
+//                      lesson-2
+// app.get('/', (req, res) => {} )
+// 1-chi qism => routing part
+// 2-chi qism => request handler
+
+// Middleware 5 xil bo'ladi
+
+// 1)                 BUILT-IN MIDDLEWARE
+
+// Ishlatilishi => 
+// 1) routing va request handler o'rtasida => (express.json())
+// 2) global holatda () => app.use(express.json())
+
+//                    APPLICAATION-LEVEL MIDDLEWARE
+
+// app.use((req, res, next) => {
+// 	if(!req.headers["authorization"] {
+// 		res.send("Unauthorized")
+// 	})
+// 	next()
+// })
+
+// Bir nechta middleware ishlatilinganda array ko'rinishida yoziladi [] => ([tokenVerify, express.json])
+
+//                   ERROR-HANDLER MIDDLEWARE
+// export const exceptionHandler = (err, req, res, next) => {
+//   res.status(err.status).send({
+//     name: err.constructor.name,
+//     message: err.message,
+//   });
+//   next();
+// };
+
+// * bu berilgan route dan har qandayi request bo'lganda xatoni ushlaydi
+
+// app.all("/*", (req, res) => {
+//   return res.status(404).json({
+//     message: "Not found",
+//     details: `${req.url.split("/")[1]} not found`,
+//   });
+// });
